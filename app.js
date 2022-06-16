eab0ca70979f8e3f57d8b11614d523a1669c93ae
johnedvalson <califaxthecool@gmail.com>
2022-06-16 13:08:42 -0600

    testing ssh

--- app.js
+++ app.js
@@ -3,11 +3,12 @@
 let gameState = {
   currentScore: 0,
   goal: 10,
-  gameStatus: 'playing',
-}
+  gameStatus: "playing",
+};
 
 // * Write functions to manipulate gameState -> Maybe even a method?
 
 // * Put Stuff of the screen
 
 // * Make them work together
+console.log("Hello");
